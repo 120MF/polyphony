85dc53b09c6d23883eca940979403362f55358ad
120MF <120MF@users.noreply.github.com>
2025-12-30 17:09:58 +0000

    Deploying to gh-pages from @ 120MF/polyphony@a0d17e0a06e771f68a1577294d99f4b40ce895e5 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767113238|6377854';
+const CACHE_VERSION = '1767114592|6746252';
 /** @type {string} */
 const CACHE_PREFIX = 'Polyphony-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
